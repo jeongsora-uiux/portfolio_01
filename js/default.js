72d6f5112bcf207953a710853e6cec0c965939f4
sorajeong <thfk2491@naver.com>
2020-04-20 18:42:13 +0900

    0420pm6:30

--- js/default.js
+++ js/default.js
@@ -47,6 +47,83 @@ $(function(){
   }
 
 
+
+
+  // ----------------------------------------------------------------
+
+  //슬라이드 2depth
+  var menu = $('.gnb > li');
+  var wrap = $('.gnbWrap');
+  var menuHeight =  menu.children('.gnb').height();
+  var pageURL = location.href;
+  var activeMenu;
+
+  menu.on({
+    mouseover: function(){
+      var tg = $(this);
+      menu.removeClass('on');
+      tg.addClass('on');
+      var th = menuHeight + tg.find('>.sGnbArea').height();
+      wrap.stop().animate({
+        height: th
+      });
+    },
+    mouseout: function(){
+      var tg = $(this);
+      menu.removeClass('on');
+      wrap.stop().animate({
+        height: menuHeight
+      });
+      onActive(); 
+    }
+  });
+
+  menu.each(function(i){
+    var tg = $(this);
+    var sub = tg.find('>.sGnbArea > ul > li');
+    var menuURL = tg.children('>.sGnbArea > ul').attr('href');
+    var active = pageURL.indexOf(menuURL);
+
+    if(active > -1){
+      activeMenu = tg;
+    }
+    sub.each(function(j){
+      var tg = $(this);
+      var subURL = tg.children('>.sGnbArea > ul > li').attr('href');
+      active = pageURL.indexOf(subURL);
+      if(active > -1){
+        activeMenu = tg;
+      }
+    });
+    sub.on({
+      mouseover: function(){
+        var tg = $(this);
+        sub.removeClass('on');
+        tg.addClass('on');
+      },
+      mouseout: function(){
+        var tg = $(this);
+        tg.removeClass('on');
+        onActive();
+      }
+    });
+  });
+  onActive();
+  function onActive(){
+    if(activeMenu){
+      activeMenu.trigger('mouseover');
+    }
+  }
+
+
+
+
+
+
+
+
+
+
   //무한 텍스트 전광판
   var OffsetWidth = $(".marquee div span").eq(0).width();
 
@@ -71,6 +148,21 @@ $(function(){
   // -----------------------------------------------------
 
   //호텔찾기 슬라이드
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 
 
