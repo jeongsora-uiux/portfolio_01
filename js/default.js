24dfdb776dc4aec8459356f581fccb5f0b13ef4d
sorajeong <thfk2491@naver.com>
2020-04-24 18:20:55 +0900

    0424pm06:20

--- js/default.js
+++ js/default.js
@@ -9,17 +9,12 @@ $(function(){
       $(".slide-bnr").slideToggle(300,rollingStart);
       $(".down").val('▲');
       sw=true;
-      console.log($(".slide-bnr").find("ul").eq(0).outerWidth());
-      // console.log($(".slide-bnr").find("ul").eq(0).outerHeight());
-      
-      console.log(rollingStart);
       clearInterval(rollingId);
       rollingId = setInterval(function(){ rollingStart();},5000);
 
     }else{
       $(".slide-bnr").slideToggle(300);
       $(".down").val('▼');
-      // $(".slide-bnr li").animate({top:70},1000);
       sw=false;
     }
   });
@@ -32,9 +27,6 @@ $(function(){
     var bannerWidth = $(".slide-bnr ul li").eq(0).outerWidth();
     var bannerHeight = $(".slide-bnr ul li").outerHeight();
     var length = $(".slide-bnr ul li").length;
-    // console.log($(".slide-bnr ul li").eq(0));
-    // console.log(bannerHeight);
-    console.log(length);
 
     $(".slide-bnr ul").css("width",bannerWidth + "px");
     $(".slide-bnr ul li").css("height",bannerHeight + "px");
@@ -45,42 +37,19 @@ $(function(){
       $(this).css("top",0);
     });
   }
-
-
-
-
   // ----------------------------------------------------------------
 
   //슬라이드 2depth
-  var menu = $('.gnb > li');
-  var wrap = $('.gnbWrap');
-  var menuHeight =  menu.children('.gnb').height();
-  var pageURL = location.href;
-  var activeMenu;
-
-
-  $(document).ready(function(){
-    var barMenu = new BarMenu("gnbWrap");
-    barMenu.setSelectMenuItemAt(1, false);
-
-    // select 이벤트 리스너 등록
-    barMenu.$barMenu.on("select",function(e){
-      var oldIndex = -1;
-      if(e.$oldItem)
-        oldIndex = e.$oldItem.index();
-    });
-    barMenu.setSelectMenuItemAt(1, false);
-  });
- 
-  //////////////////////---------------------
-
-  
   $('.gnb_01').on({
     mouseenter:function(){
       $(this).find('div').slideDown();
+      $(this).addClass('menu__item--current');
+      $(this).find('a').addClass('menu__item--current');
     },
     mouseleave:function(){
       $(this).find('div').slideUp();
+      $('.gnb_01').removeClass('menu__item--current');
+      $('.gnb_01').find('a').removeClass('menu__item--current');
     },
     click:function(){
       $('.gnb_01').removeClass('menu__item--current');
@@ -90,20 +59,13 @@ $(function(){
       $(this).find('a').addClass('menu__item--current');
     }
   })
-
   //////////////////////---------------------
-
 
   //무한 텍스트 전광판
   var OffsetWidth = $(".marquee div span").eq(0).width();
 
-  console.log('a');
-  console.log(OffsetWidth);  //3175.92
-
   var cnt = 0;
- 
   var timer = setInterval(function(){
-    
     if(cnt != OffsetWidth){
       cnt++;
       //clearInterval(timer);
@@ -112,25 +74,42 @@ $(function(){
     }
     $('.marquee div').css({left:-cnt});
   },0);
-  
 
 
   // -----------------------------------------------------
 
   //호텔찾기 슬라이드
-  
+  var time; 
+  var $carouselLi;
+  var carouselCount; 
+  var currentIndex; 
+  var caInterval;
+  var imgW;    
+  $(document).ready(function(){
+      carouselInit(2500, 5000);
+  });
+  function carouselInit( height ){
+      $carousel = $(".slide_box");
+      $carouselLi = $(".slide");
+      carouselCount = $carouselLi.length; 
+      currentIndex = 0; 
+      imgW=$carouselLi.width();
+      carousel();
+  }
 
+  function carousel(){
+      caInterval = setInterval(function(){
+          var left = "-" + imgW;  //384  
+          $carousel.animate( {left: left * currentIndex},1000, function(){
+            $('.slide').first().appendTo("<li>" + $('.slide').find("li:first").html() + "</li>");
+            $('.slide').find("li:first").remove();
+            $('.slide').css("left","0px");
+            currentIndex++;
+          });
+      }, 1000);
+  };
 
-
-
-
-
-
-
-
-
-
-
+ 
 
 
   
